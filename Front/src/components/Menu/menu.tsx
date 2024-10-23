3272ace681a4dc25e4a547db7d5b8de264888765
gustavoreislima13 · he/him <resanselmo@gmail.com>
2024-10-23 00:00:14 -0300

    home arrumado

--- Front/src/components/Menu/menu.tsx
+++ Front/src/components/Menu/menu.tsx
@@ -5,8 +5,8 @@ export default function Menu() {
   return (
     
       <ul className="flex space-x-8">
-        <li><Link href="/" className="text-white text-xl hover:text-gray-200">Home</Link></li>
-        <li><Link href="/temas/tema1" className="text-white text-xl hover:text-gray-200">Serviços</Link></li>
+        <li><Link href="/temas/tema1" className="text-white text-xl hover:text-gray-200">Home</Link></li>
+        <li><Link href="/" className="text-white text-xl hover:text-gray-200">Serviços</Link></li>
         <li><Link href="/temas/tema2" className="text-white text-xl hover:text-gray-200">Contato</Link></li>
         <li><Link href="/temas/tema3" className="text-white text-xl hover:text-gray-200">Login</Link></li>
               </ul>
