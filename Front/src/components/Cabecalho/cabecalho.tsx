11aa2ed8fbb18edcf60e871ebd541caa2225481d
gustavoreislima13 · he/him <resanselmo@gmail.com>
2024-10-22 23:58:43 -0300

    rodape,men, cabecalho

--- Front/src/components/Cabecalho/cabecalho.tsx
+++ Front/src/components/Cabecalho/cabecalho.tsx
@@ -1,10 +1,18 @@
-import Menu from "../Menu/menu";
+import Menu from '../Menu/menu';
+import Image from 'next/image';
 
-export default function Cabecalho(){
-    return(
-        <header className="bg-blue-500 h-40 flex flex-col items-center justify-center py-4">
-            
-            <div><Menu/></div>
-        </header> 
-    )
-};
+export default function Cabecalho() {
+  return (
+    <header className="flex justify-between items-center p-6 bg-gradient-to-r from-white to-blue-500 w-full">
+      <div className="flex items-center">
+        <Image src="/porto_seguro_logo.png" alt="Porto Seguro Logo" width={50} height={50} />
+      </div>
+      <div>
+        <h1 className="text-black text-3xl font-bold"></h1>
+      </div>
+      <div>
+        <Menu />
+      </div>
+    </header>
+  );
+}
--- Front/src/components/Rodape/rodape.tsx
+++ Front/src/components/Rodape/rodape.tsx
@@ -1,7 +1,40 @@
-export default function Rodape(){
-    return (
-    <footer className="bg-blue-500 h-30 flex flex-col items-center justify-center py-4">
-        <h1 className="text-white text-xl hover:text-gray-200">Feito por Julia, Leonardo e Gustavo</h1>
+import Image from 'next/image';
+
+export default function Footer() {
+  return (
+    <footer className="flex justify-between items-start p-8 bg-white w-full border-t border-gray-200">
+      <div className="flex-1">
+        <Image src="/porto_seguro_logo.png" alt="Porto Seguro Logo" width={50} height={50} />
+        <p className="mt-4 text-gray-800 max-w-xs">
+          Conte com todo nosso cuidado também, cartões e serviços Porto em um só lugar de forma rápida e segura. Baixe agora!
+        </p>
+      </div>
+      <div className="flex-1">
+        <h3 className="text-lg font-bold text-gray-900 mb-4">Rede social</h3>
+        <ul className="text-gray-800 space-y-2">
+          <li>Instagram</li>
+          <li>Facebook</li>
+          <li>Site</li>
+          <li>Whatsapp</li>
+        </ul>
+      </div>
+      <div className="flex-1">
+        <h3 className="text-lg font-bold text-gray-900 mb-4">Paginas</h3>
+        <ul className="text-gray-800 space-y-2">
+          <li>Home</li>
+          <li>Menu</li>
+          <li>Serviços</li>
+          <li>Contato</li>
+        </ul>
+      </div>
+      <div className="flex-1">
+        <h3 className="text-lg font-bold text-gray-900 mb-4">Rm dos alunos</h3>
+        <ul className="text-gray-800 space-y-2">
+          <li>556857</li>
+          <li>555672</li>
+          <li>555708</li>
+        </ul>
+      </div>
     </footer>
-    )
+  );
 }
